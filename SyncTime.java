ce4278c4e7f704388cdeb373ff9c37c76dedba16
Siddarth Senthilkumar <ssenthilkumar3@gatech.edu>
2015-02-11 19:09:02 -0500

    Made checkstyle-acceptable
    This program was initially written before I knew proper style.

--- SyncTime.java
+++ SyncTime.java
@@ -1,53 +1,68 @@
-//From http://www.rgagnon.com/javadetails/java-0589.html
-//Uses method to return a gregorianCalendar
-//gregorianCalendar is changed into a simple date format in atomicClock class and read as a string
-import java.io.*;
-import java.util.*;
-import java.net.*;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+import java.net.URLConnection;
+import java.net.URL;
 
+/**
+ * This class receives the current time from an atomic clock
+ * server and parses the data into GregorianCalendar format.
+ *
+ * Adapted from:
+ * http://www.rgagnon.com/javadetails/java-0589.html
+ *
+ * @version 1.0
+ */
 public final class SyncTime {
-  // NIST, Boulder, Colorado  (time-a.timefreq.bldrdoc.gov)
- public static final String ATOMICTIME_SERVER="http://132.163.4.101:13";
- 
-  public final static GregorianCalendar getAtomicTime() throws IOException{
-    BufferedReader in = null;
+    // NIST, Boulder, Colorado  (time-a.timefreq.bldrdoc.gov)
+    private static final String ATOMICTIME_SERVER = "http://132.163.4.101:13";
 
-    try {
-       URLConnection conn = new URL(ATOMICTIME_SERVER).openConnection();
-       in = new BufferedReader
-         (new InputStreamReader(conn.getInputStream()));
+    /**
+     * Reads in time data from an online server.
+     * Parses data into GregorianCalendar format
+     * @return The GregorianCalendar object containing the current time and date
+     * @throws java.io.IOException Error when we cannot read properly parse data
+     */
+    public final GregorianCalendar getAtomicTime() throws IOException {
+        BufferedReader in = null;
 
-       String atomicTime;
-       while (true) {
-          if ( (atomicTime = in.readLine()).indexOf("*") > -1) {
-             break;
-          }
-       }
-       String[] fields = atomicTime.split(" ");
-       GregorianCalendar calendar = new GregorianCalendar();
+        try {
+            URLConnection conn = new URL(ATOMICTIME_SERVER).openConnection();
+            in = new BufferedReader(
+                 new InputStreamReader(conn.getInputStream()));
 
-       String[] date = fields[1].split("-");
-       calendar.set(Calendar.YEAR, 2000 +  Integer.parseInt(date[0]));
-       calendar.set(Calendar.MONTH, Integer.parseInt(date[1])-1);
-       calendar.set(Calendar.DATE, Integer.parseInt(date[2]));
+            String atomicTime;
+            while (true) {
+                if ((atomicTime = in.readLine()).indexOf("*") > -1) {
+                    break;
+                }
+            }
+            String[] fields = atomicTime.split(" ");
+            GregorianCalendar calendar = new GregorianCalendar();
 
-       // deals with the timezone and the daylight-saving-time
-       TimeZone tz = TimeZone.getDefault();
-       int gmt = (tz.getRawOffset() + tz.getDSTSavings()) / 3600000;
+            String[] date = fields[1].split("-");
+            calendar.set(Calendar.YEAR, 2000 +  Integer.parseInt(date[0]));
+            calendar.set(Calendar.MONTH, Integer.parseInt(date[1]) - 1);
+            calendar.set(Calendar.DATE, Integer.parseInt(date[2]));
 
-       String[] time = fields[2].split(":");
-       calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(time[0]) + gmt);
-       calendar.set(Calendar.MINUTE, Integer.parseInt(time[1]));
-       calendar.set(Calendar.SECOND, Integer.parseInt(time[2]));
-       return calendar;
+            // deals with the timezone and the daylight-saving-time
+            TimeZone tz = TimeZone.getDefault();
+            int gmt = (tz.getRawOffset() + tz.getDSTSavings()) / 3600000;
+
+            String[] time = fields[2].split(":");
+            calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(time[0]) + gmt);
+            calendar.set(Calendar.MINUTE, Integer.parseInt(time[1]));
+            calendar.set(Calendar.SECOND, Integer.parseInt(time[2]));
+            return calendar;
+        } catch (IOException e) {
+            throw e;
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
     }
-    catch (IOException e){
-       throw e;
-    }
-    finally {
-       if (in != null) {
-         in.close();
-       }
-    }
-  }
 }
